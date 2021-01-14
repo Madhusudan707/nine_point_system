6ce271f01d79d69ede39170208d46715d60c9b2f
xdev200 <xdev200200@gmail.com>
2021-01-14 12:15:25 +0530

    update: question and answer direction

--- markOne_QuizAboutMe/index.js
+++ markOne_QuizAboutMe/index.js
@@ -2,36 +2,39 @@ const readlineSync = require('readline-sync');
 const chalk = require('chalk')
 console.log(chalk.yellow.bold("Do You Know About Me?\n"))
 
-function QuizAboutMe(questions,answer){
-  let response = readlineSync.question(chalk.yellow("\n"+questions))
+console.log(chalk.yellow.bold("Write your answer in small letter\n"))
 
-  if(answer === response){
-    console.log(chalk.green ("\nCorrect"))
-  }else{
-    console.log(chalk.red (" \nWrong"))
+function QuizAboutMe(questions, answer) {
+  let response = readlineSync.question(chalk.yellow("\n" + questions))
+
+  if (answer === response) {
+    console.log(chalk.green("\nCorrect"))
+  } else {
+    console.log(chalk.red(" \nWrong"))
   }
-  
+
 }
 
-qa_arr =[
+qa_arr = [
   {
-      q: "What is my Nickname? ",
-      a: "Maddy"
+    q: "What is my Nickname? ",
+    a: "maddy"
   },
-   {
-      q: "How Old Am I? ",
-      a: "32"
+  {
+    q: "My favourite Color? ",
+    a: "blue"
   },
-   {
-      q: "Chess is My favourite Game, Yes or No? ",
-      a: "Yes"
+  {
+    q: "Chess is My favourite Game, Yes or No? ",
+    a: "yes"
   },
-   {
-      q: "I am Single, Yes or No ",
-      a: "Yes"
+  {
+    q: "Which Marvel Character I like most? ",
+    a: "ironman"
   }
 ]
 
-for(let i =0; i<qa_arr.length;i++){
- QuizAboutMe(qa_arr[i].q,qa_arr[i].a)
+for (let i = 0; i < qa_arr.length; i++) {
+  QuizAboutMe(qa_arr[i].q, qa_arr[i].a)
 }
+
